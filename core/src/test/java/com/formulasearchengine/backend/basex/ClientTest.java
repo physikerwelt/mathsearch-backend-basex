1aa48f957d804fb41f3c54e2aefe5bdc731f43d6
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-03-13 09:05:06 -0400

    2nd chance for restx
    Change-Id: Id811ee31dd955e0f435604cb3d854e2c14ea31f5

--- core/src/test/java/com/formulasearchengine/backend/basex/ClientTest.java
+++ core/src/test/java/com/formulasearchengine/backend/basex/ClientTest.java
@@ -11,30 +11,29 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class ClientTest extends TestCase {
+	@Test
+	public void testSequential() throws Exception {
+		basicTest();
+		MWSQuery();
+		MWS2();
+	}
+
 	@BeforeClass
-	private static void setup () throws Exception {
+	private static void setup() throws Exception {
 		Client c = new Client();
-		String res =c.execute( "let $message := 'Hello World!'\n" +
-				"return\n" +
-				"<results>\n" +
-				"   <message>{$message}</message>\n" +
-				"</results>" );
+		String res = c.execute( "let $message := 'Hello World!'\n" +
+			"return\n" +
+			"<results>\n" +
+			"   <message>{$message}</message>\n" +
+			"</results>" );
 		if ( res.contains( "failed XQJNC001 - Connection refused: connect" ) ) {
 			System.out.println( res );
 			(new ServerTest()).testImportData();
 		}
 	}
-	@SuppressWarnings("SameParameterValue")
-	static public String getFileContents (String fname) throws IOException {
-		try (InputStream is = ClientTest.class.getClassLoader().getResourceAsStream(fname)) {
-			final Scanner s = new Scanner(is, "UTF-8");
-			//Stupid scanner tricks to read the entire file as one token
-			s.useDelimiter("\\A");
-			return s.hasNext() ? s.next() : "";
-		}
-	}
+
 	@Ignore
-	public void basicTest () throws Exception {
+	public void basicTest() throws Exception {
 		setup();
 		Client c = new Client();
 		String res = c.execute( "declare default element namespace \"http://www.w3.org/1998/Math/MathML\";\n" +
@@ -45,7 +44,10 @@ public class ClientTest extends TestCase {
 			"fn:count($x/*) = 3\n" +
 			"return\n" +
 			"<result>{$m/@url}</result>" );
-		assertEquals( res.replaceFirst( "runtime=\"\\d+\"","" ), "    <result for=\"NTCIR11-Math-\" >\n" +
+		res = res.replaceFirst( "runtime=\"\\d+\"", "" );
+		assertEquals( "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+			"<results xmlns=\"http://ntcir-math.nii.ac.jp/\" total=\"37\">\n" +
+			"    <result for=\"NTCIR11-Math-\" >\n" +
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"math.4.5\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"1\"/>\n" +
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"math.4.5\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"2\"/>\n" +
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"math.5.2\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"3\"/>\n" +
@@ -83,26 +85,33 @@ public class ClientTest extends TestCase {
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"math.8.34\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"35\"/>\n" +
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"math.8.35\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"36\"/>\n" +
 			"      <hit id=\"<result xmlns=\"http://www.w3.org/1998/Math/MathML\" url=\"dummy29\"/>\" xref=\"math000000000000.xml\" score=\"10\" rank=\"37\"/>\n" +
-			"    </result>\n" );
-	}
-	@Ignore
-	public void MWSQuery () throws Exception {
-		setup();
-		final String testInput = getFileContents( "dummy29.xml" );
-		final String expectedOutput = "    <result for=\"NTCIR11-Math-\" >\n" +
-			"      <hit id=\"dummy29\" xref=\"math000000000000.xml\" score=\"10\" rank=\"1\"/>\n" +
-			"    </result>\n";
-		Document query = XMLHelper.String2Doc(testInput);
-		Client c = new Client();
-		String res = c.runMWSQuery( query );
-		assertEquals( expectedOutput, res.replaceAll( "runtime=\"\\d+\"" , "" ) );
+			"    </result>\n" +
+			"</results>\n", res );
 	}
 
 	@Ignore
-	public void MWS2 () throws  Exception{
+	public void MWSQuery() throws Exception {
+		setup();
+		final String testInput = getFileContents( "dummy29.xml" );
+		final String expectedOutput = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+			"<results xmlns=\"http://ntcir-math.nii.ac.jp/\" total=\"1\">\n" +
+			"    <result for=\"NTCIR11-Math-\" >\n" +
+			"      <hit id=\"dummy29\" xref=\"math000000000000.xml\" score=\"10\" rank=\"1\"/>\n" +
+			"    </result>\n"+
+			"</results>\n";
+		Document query = XMLHelper.String2Doc( testInput );
+		Client c = new Client();
+		String res = c.runMWSQuery( query );
+		assertEquals( expectedOutput, res.replaceAll( "runtime=\"\\d+\"", "" ) );
+	}
+
+	@Ignore
+	public void MWS2() throws Exception {
 		setup();
 		final String testInput = getFileContents( "mws.xml" );
-		final String expectedOutput = "    <result for=\"NTCIR11-Math-\" >\n" +
+		final String expectedOutput = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+			"<results xmlns=\"http://ntcir-math.nii.ac.jp/\" total=\"7\">\n" +
+			"    <result for=\"NTCIR11-Math-\" >\n" +
 			"      <hit id=\"math.8.13\" xref=\"math000000000000.xml\" score=\"10\" rank=\"1\"/>\n" +
 			"      <hit id=\"math.8.22\" xref=\"math000000000000.xml\" score=\"10\" rank=\"2\"/>\n" +
 			"      <hit id=\"math.8.23\" xref=\"math000000000000.xml\" score=\"10\" rank=\"3\"/>\n" +
@@ -110,17 +119,21 @@ public class ClientTest extends TestCase {
 			"      <hit id=\"math.8.25\" xref=\"math000000000000.xml\" score=\"10\" rank=\"5\"/>\n" +
 			"      <hit id=\"math.8.30\" xref=\"math000000000000.xml\" score=\"10\" rank=\"6\"/>\n" +
 			"      <hit id=\"math.8.32\" xref=\"math000000000000.xml\" score=\"10\" rank=\"7\"/>\n" +
-			"    </result>\n";
+			"    </result>\n"+
+			"</results>\n";
 		Document query = XMLHelper.String2Doc( testInput );
 		Client c = new Client();
 		String res = c.runMWSQuery( query );
-		assertEquals( expectedOutput, res.replaceAll( "runtime=\"\\d+\"" , "" ) );
+		assertEquals( expectedOutput, res.replaceAll( "runtime=\"\\d+\"", "" ) );
 	}
-	@Test
-	public void testSequential() throws  Exception{
-		setup();
-		basicTest();
-		MWSQuery();
-		MWS2();
+
+	@SuppressWarnings("SameParameterValue")
+	static public String getFileContents( String fname ) throws IOException {
+		try ( InputStream is = ClientTest.class.getClassLoader().getResourceAsStream( fname ) ) {
+			final Scanner s = new Scanner( is, "UTF-8" );
+			//Stupid scanner tricks to read the entire file as one token
+			s.useDelimiter( "\\A" );
+			return s.hasNext() ? s.next() : "";
+		}
 	}
 }
