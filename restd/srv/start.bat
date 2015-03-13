f34de4339832ecf913a1341f761b8bd72d5f0567
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2015-03-13 09:05:06 -0400

    2nd chance for restx
    Change-Id: Id811ee31dd955e0f435604cb3d854e2c14ea31f5

---
+++ restd/srv/start.bat
@@ -0,0 +1,1 @@
+java -cp "target/restx/classes;target/dependency/*" -Drestx.app.package=com\formulasearchengine\backend\basex -Drestx.mode=prod %VM_OPTIONS%  com\formulasearchengine\backend\basex.AppServer
